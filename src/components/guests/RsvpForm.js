d48fd0e051b80f8861914cb8173b181e974ea100
torianne02 <victoria.fluharty@gmail.com>
2019-01-25 13:20:50 -0800

    continues adding to rsvp form

--- src/components/guests/RsvpForm.js
+++ src/components/guests/RsvpForm.js
@@ -5,6 +5,14 @@ class RsvpForm extends Component {
   render() {
     return (
       <Form>
+        <FormGroup>
+          <Label for="guest-name">Name</Label>
+          <Input type="text" name="guest-name" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="guest-email">Email</Label>
+          <Input type="email" name="guest-email" />
+        </FormGroup>
         <FormGroup tag="fieldset">
           <legend>Will your party be attending?</legend>
           <FormGroup check>
