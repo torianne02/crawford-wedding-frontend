baacd5463f57f92836cb6f597c55733cfd681734
torianne02 <victoria.fluharty@gmail.com>
2019-01-25 12:43:13 -0800

    adds to-do comment in code

--- src/containers/GuestListContainer.js
+++ src/containers/GuestListContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// will need to import Guests component
 
 class GuestList extends Component {
   constructor(props) {
