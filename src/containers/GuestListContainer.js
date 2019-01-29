65d298a25557d89f4960c3134a8271c0de0cca51
torianne02 <victoria.fluharty@gmail.com>
2019-01-29 11:17:52 -0800

    continues testing cases for GuestListContainer

--- src/containers/GuestListContainer.js
+++ src/containers/GuestListContainer.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 // import Guests from '../components/guests/Guests';
 // import RsvpForm from '../components/guests/RsvpForm';
+// import { fetchAllRsvps } from '../actions/rsvpActions';
 
 class GuestList extends Component {
   constructor(props) {
     super(props);
-
+    console.log('in constructor')
     this.state = {
-      guests: []
+      guests: ['Tori', 'Kevin']
     };
   }
 
   componentDidMount() {
     // need to write fetch statement
+    console.log("in component did mount")
+    console.log(this.state)
   }
 
   // need to have methods for onSubmit and onChange of form
@@ -22,7 +25,7 @@ class GuestList extends Component {
     return (
       "GuestList Container entered!"
     )
-    // return <Guests guests={this.state.books} />
+    // return <Guests guests={this.state.guests} />
     // need to render form
   }
 }
