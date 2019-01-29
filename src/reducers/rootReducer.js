1306e904f9c8450a7f3aac055ed01c8828114713
torianne02 <victoria.fluharty@gmail.com>
2019-01-29 13:41:25 -0800

    adds return statements for rootReducer switch cases

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,22 +2,20 @@ export default function rootReducer( state = {
   isFetchingRsvp: false,
   rsvp: [],
   rsvps: [],
-  // need something in regards to requesting rsvp
+  rsvpRequestInProgress: false,
 }, action ) {
   switch ( action.type ) {
     case 'FETCH_RSVP':
-      // return statement to get rsvp from API
-      break;
+      return { ...state, isFetchingRsvp: true, rsvp: [] }
     case 'ADD_RSVP_TO_STATE':
-      // return statement adding rsvp to state
-      break;
+      return { isFetchingRsvp: false, rsvpRequestInProgress: false, rsvp: action.data }
     case 'FETCH_ALL_RSVPS':
-      // return statement to get all rsvps from API
-      break;
+      return { isFetchingRsvp: true, rsvps: [] }
     case 'ADD_ALL_RSVPS_TO_STATE':
-      // return statement adding all rsvps to state
-      break;
-    // need 'POST' for RSVP - 'RSVP_REQUEST' && 'ADD_RSVP_TO_STATE'
+      return { isFetchingRsvp: false, rsvps: action.data }
+    case 'RSVP_REQUEST':
+      return { ...state, rsvpRequestInProgress: true }
+
     default:
       return state;
   }
