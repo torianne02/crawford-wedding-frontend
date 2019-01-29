514f981783e510cb2a53271bdc0b78ea9484ed41
torianne02 <victoria.fluharty@gmail.com>
2019-01-29 11:16:43 -0800

    adds break to switch statement

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -7,12 +7,16 @@ export default function rootReducer( state = {
   switch ( action.type ) {
     case 'FETCH_RSVP':
       // return statement to get rsvp from API
+      break;
     case 'ADD_RSVP_TO_STATE':
       // return statement adding rsvp to state
+      break;
     case 'FETCH_ALL_RSVPS':
       // return statement to get all rsvps from API
+      break;
     case 'ADD_ALL_RSVPS_TO_STATE':
       // return statement adding all rsvps to state
+      break;
     // need 'POST' for RSVP - 'RSVP_REQUEST' && 'ADD_RSVP_TO_STATE'
     default:
       return state;
