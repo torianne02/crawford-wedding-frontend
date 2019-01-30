9efe4a025d11058d0d96caadf6701689db3de249
torianne02 <victoria.fluharty@gmail.com>
2019-01-30 10:16:56 -0800

    updating form hides labels and adds placeholders adds rows and columns rearranges form inputs changes attendees type to number

--- src/components/rsvps/RsvpForm.js
+++ src/components/rsvps/RsvpForm.js
@@ -1,54 +1,76 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, Col, Row } from 'reactstrap';
+import {  Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const RsvpForm = props => {
 
   return (
     <Form onSubmit={event => this.handleSubmit(event)}>
       <Row form>
-        <Col md={6}>
+        <Col md={3}>
           <FormGroup>
-            <Label for="name">Name</Label>
-            <Input type="text" name="name" onChange={ props.handleNameOnChange }
+            <Label for="name" hidden>Name</Label>
+            <Input type="text"
+              name="name"
+              placeholder="Full Name"
+              onChange={ props.handleNameOnChange }
               value={ props.nameValue } />
           </FormGroup>
         </Col>
-        <Col md={6}>
+        <Col md={3}>
           <FormGroup>
-            <Label for="email">Email</Label>
-            <Input type="email" name="email" onChange={ props.handleEmailOnChange }
+            <Label for="email" hidden>Email</Label>
+            <Input type="email"
+              name="email"
+              placeholder="Email"
+              onChange={ props.handleEmailOnChange }
               value={ props.emailValue } />
           </FormGroup>
         </Col>
       </Row>
-      <FormGroup>
-        <Label for="attendees">Attendees</Label>
-        <Input type="attendees" name="attendees" onChange={ props.handleAttendeesOnChange }
-          value={ props.attendeesValue } />
-      </FormGroup>
+      <Row form>
+        <Col md={3}>
+          <FormGroup>
+            <Label for="attendees" hidden>Number in Party</Label>
+            <Input type="number"
+              name="attendees"
+              placeholder="Number in Party"
+              onChange={ props.handleAttendeesOnChange }
+              value={ props.attendeesValue } />
+          </FormGroup>
+        </Col>
+        <Col md={3}>
+          <FormGroup>
+            <Label for="songRequest" hidden>Song Request</Label>
+            <Input type="text"
+              name="songRequest"
+              placeholder="Song Request"
+              onChange={ props.handleSongRequestOnChange }
+              value={ props.songRequestValue } />
+          </FormGroup>
+        </Col>
+      </Row>
       <FormGroup tag="fieldset">
         <legend className="col-form-label col-sm-2">Will your party be attending?</legend>
         <Col sm={10}>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="accept" onChange={ props.handleAcceptOnChange }
-                value={ props.acceptValue } />{' '}
+              <Input type="radio"
+                name="accept"
+                onChange={ props.handleAcceptOnChange }
+                value="accepts" checked={props.acceptValue === "accepts"} />{' '}
               Accepts with pleasure.
             </Label>
           </FormGroup>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="decline" onChange={ props.handleAcceptOnChange }
-                value={ props.acceptValue } />{' '}
+              <Input type="radio"
+                name="accept"
+                onChange={ props.handleAcceptOnChange }
+                value="declines" checked={props.acceptValue === "declines"} />{' '}
               Declines with regret.
             </Label>
           </FormGroup>
         </Col>
-      </FormGroup>
-      <FormGroup>
-        <Label for="song-request">Song Request</Label>
-        <Input type="text" name="song-request" onChange={ props.handleSongRequestOnChange }
-          value={ props.songRequestValue } />
       </FormGroup>
       <Button>Submit</Button>
     </Form>
