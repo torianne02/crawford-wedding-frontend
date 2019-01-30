a65c33f51ce062e26f79a25c10c91ead50dc81c4
torianne02 <victoria.fluharty@gmail.com>
2019-01-30 10:39:38 -0800

    removes rows from form aligns radio buttons to the left in index.css adds container to wrap form

--- src/components/rsvps/RsvpForm.js
+++ src/components/rsvps/RsvpForm.js
@@ -1,79 +1,77 @@
 import React from 'react';
-import {  Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Container, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const RsvpForm = props => {
 
   return (
-    <Form onSubmit={event => this.handleSubmit(event)}>
-      <Row form>
-        <Col md={3}>
-          <FormGroup>
-            <Label for="name" hidden>Name</Label>
-            <Input type="text"
-              name="name"
-              placeholder="Full Name"
-              onChange={ props.handleNameOnChange }
-              value={ props.nameValue } />
-          </FormGroup>
-        </Col>
-        <Col md={3}>
-          <FormGroup>
-            <Label for="email" hidden>Email</Label>
-            <Input type="email"
-              name="email"
-              placeholder="Email"
-              onChange={ props.handleEmailOnChange }
-              value={ props.emailValue } />
-          </FormGroup>
-        </Col>
-      </Row>
-      <Row form>
-        <Col md={3}>
-          <FormGroup>
-            <Label for="attendees" hidden>Number in Party</Label>
-            <Input type="number"
-              name="attendees"
-              placeholder="Number in Party"
-              onChange={ props.handleAttendeesOnChange }
-              value={ props.attendeesValue } />
-          </FormGroup>
-        </Col>
-        <Col md={3}>
-          <FormGroup>
-            <Label for="songRequest" hidden>Song Request</Label>
-            <Input type="text"
-              name="songRequest"
-              placeholder="Song Request"
-              onChange={ props.handleSongRequestOnChange }
-              value={ props.songRequestValue } />
-          </FormGroup>
-        </Col>
-      </Row>
-      <FormGroup tag="fieldset">
-        <legend className="col-form-label col-sm-2">Will your party be attending?</legend>
-        <Col sm={10}>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio"
-                name="accept"
-                onChange={ props.handleAcceptOnChange }
-                value="accepts" checked={props.acceptValue === "accepts"} />{' '}
-              Accepts with pleasure.
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio"
-                name="accept"
-                onChange={ props.handleAcceptOnChange }
-                value="declines" checked={props.acceptValue === "declines"} />{' '}
-              Declines with regret.
-            </Label>
-          </FormGroup>
-        </Col>
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
+    <Container className="RSVP">
+      <Form onSubmit={event => this.handleSubmit(event)}>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="name" hidden>Name</Label>
+              <Input type="text"
+                name="name"
+                placeholder="Full Name"
+                onChange={ props.handleNameOnChange }
+                value={ props.nameValue } />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="email" hidden>Email</Label>
+              <Input type="email"
+                name="email"
+                placeholder="Email"
+                onChange={ props.handleEmailOnChange }
+                value={ props.emailValue } />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="attendees" hidden>Number in Party</Label>
+              <Input type="number"
+                name="attendees"
+                placeholder="Number in Party"
+                onChange={ props.handleAttendeesOnChange }
+                value={ props.attendeesValue } />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="songRequest" hidden>Song Request</Label>
+              <Input type="text"
+                name="songRequest"
+                placeholder="Song Request"
+                onChange={ props.handleSongRequestOnChange }
+                value={ props.songRequestValue } />
+            </FormGroup>
+          </Col>
+        <FormGroup tag="fieldset">
+          <legend className="col-form-label col-md-6">Will your party be attending?</legend>
+          <Col md={6}>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio"
+                  name="accept"
+                  onChange={ props.handleAcceptOnChange }
+                  value="accepts" checked={props.acceptValue === "accepts"} />{' '}
+                Accepts with pleasure.
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio"
+                  name="accept"
+                  onChange={ props.handleAcceptOnChange }
+                  value="declines" checked={props.acceptValue === "declines"} />{' '}
+                Declines with regret.
+              </Label>
+            </FormGroup>
+          </Col>
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    </Container>
   );
 }
 
