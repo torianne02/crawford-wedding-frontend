daf73ca58c81de25e4604559b2704b4f5d0272ca
torianne02 <victoria.fluharty@gmail.com>
2019-01-30 12:50:27 -0800

    uses combineReducers to create rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,22 +1,8 @@
-export default function rootReducer( state = {
-  isFetchingRsvp: false,
-  rsvp: [],
-  rsvps: [],
-  rsvpRequestInProgress: false,
-}, action ) {
-  switch ( action.type ) {
-    case 'FETCH_RSVP':
-      return { ...state, isFetchingRsvp: true, rsvp: [] }
-    case 'ADD_RSVP_TO_STATE':
-      return { isFetchingRsvp: false, rsvpRequestInProgress: false, rsvp: action.data }
-    case 'FETCH_ALL_RSVPS':
-      return { isFetchingRsvp: true, rsvps: [] }
-    case 'ADD_ALL_RSVPS_TO_STATE':
-      return { isFetchingRsvp: false, rsvps: action.data }
-    case 'RSVP_REQUEST':
-      return { ...state, rsvpRequestInProgress: true }
+import { combineReducers } from 'redux';
+import rsvps from './rsvps';
+import guestList from './guestList';
 
-    default:
-      return state;
-  }
-}
+export default combineReducers({
+  rsvps,
+  guestList
+})
