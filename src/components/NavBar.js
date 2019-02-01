2d636b3b398163c010d1e5672c4e8966a4a23c11
torianne02 <victoria.fluharty@gmail.com>
2019-02-01 11:20:40 -0800

    continues work on NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,18 @@ import { Navbar, NavbarBrand, NavItem, NavLink, Nav } from 'reactstrap';
 
 const NavBar = () => {
   return (
-    <Navbar color="light">
+    <Navbar>
       <NavbarBrand href="/">Crawford Wedding</NavbarBrand>
       <Nav className="nav">
-        
+        <NavItem>
+          <NavLink href="/attending">Guest List</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/rsvp">RSVP</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/details">Details</NavLink>
+        </NavItem>
       </Nav>
     </Navbar>
   )
