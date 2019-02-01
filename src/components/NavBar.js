546067a12759e27bf19f6b228f9eb265f6e4fb93
torianne02 <victoria.fluharty@gmail.com>
2019-02-01 10:47:34 -0800

    adds NavBar.js - work in progress

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, NavbarBrand, NavItem, NavLink, Nav } from 'reactstrap';
+
+const NavBar = () => {
+  return (
+    <Navbar color="light">
+      <NavbarBrand href="/">Crawford Wedding</NavbarBrand>
+      <Nav className="nav">
+        
+      </Nav>
+    </Navbar>
+  )
+}
+
+export default NavBar
