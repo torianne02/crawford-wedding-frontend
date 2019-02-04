39455ff2fc56f2981f45dbf384e7385b1abd126d
torianne02 <victoria.fluharty@gmail.com>
2019-02-04 15:06:56 -0800

    fixes syntax in rootReducer now exports rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 import rsvps from './rsvps';
 import guestList from './guestList';
 
-export default combineReducers({
+const rootReducer = combineReducers({
   rsvps,
   guestList
-})
+});
+
+export default rootReducer
