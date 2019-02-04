1a69fa943eeca5e413042e57f50080eca3a2ea51
torianne02 <victoria.fluharty@gmail.com>
2019-02-04 15:33:00 -0800

    adds __REDUX_DEVTOOLS_EXTENSION to store

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store} >
