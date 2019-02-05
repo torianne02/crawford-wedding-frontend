855195e625109edea01ba998e75ad33d5c3fd837
torianne02 <victoria.fluharty@gmail.com>
2019-02-05 10:01:07 -0800

    changes state in guestList reducer also removes one action

--- src/reducers/guestList.js
+++ src/reducers/guestList.js
@@ -1,12 +1,9 @@
 export default function rsvps( state = {
-  isFetchingGuestList: false,
   guestList: [],
 }, action ) {
   switch ( action.type ) {
     case 'FETCH_GUEST_LIST':
-      return { ...state, isFetchingGuestList: true, guestList: [] }
-    case 'ADD_GUEST_LIST_TO_STATE':
-      return { isFetchingGuestList: false, guestList: action.data }
+      return { ...state, guestList: action.data }
 
     default:
       return state;
