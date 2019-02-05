6d7847eadec7c7df88551eaa2bc02f3961e28508
torianne02 <victoria.fluharty@gmail.com>
2019-02-05 09:19:34 -0800

    removes window devtools from store due to error

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
 const store = createStore(
   rootReducer,
-  compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk),
-  )
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render(
