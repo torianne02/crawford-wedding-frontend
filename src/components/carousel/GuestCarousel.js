1d8b700b9b1894694f9832f02b7a39d84de79544
torianne02 <victoria.fluharty@gmail.com>
2019-02-05 13:14:09 -0800

    creates GuestCarousel component

---
+++ src/components/carousel/GuestCarousel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const GuestCarousel = ( props ) => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default GuestCarousel
