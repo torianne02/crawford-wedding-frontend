18838fb03493f4ab54fb1fa453bd14432d3b844d
torianne02 <victoria.fluharty@gmail.com>
2019-02-04 16:47:58 -0800

    adds mapDispatchToProps and mapStateToProps to MusicContainer

--- src/containers/MusicContainer.js
+++ src/containers/MusicContainer.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import Music from '../components/music/Music';
-import { fetchGuestList } from '../actions/guestListActions';
+import { fetchAllRsvps } from '../actions/rsvpActions';
 
 class MusicContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      guestList: []
+      rsvps: []
     };
   }
 
-  // get all song requests from all rsvps -- use fetchAllRsvps
+  componentDidMount() {
+    this.props.fetchAllRsvps()
+  }
 
   render() {
     debugger
     return (
       <div className="guestList">
         <h1>Requested Songs</h1>
-        <Music guestList={ this.state.guestList }/>
+        <Music rsvps={ this.state.rsvps }/>
       </div>
     )
   }
 }
 
-// function mapDispatchToProps( dispatch ) {
-//   return { fetchGuestList: () => dispatch(fetchGuestList()) }
-// }
-//
-// function mapStateToProps( state ) {
-//   return { guests: state.guests }
-// }
+function mapDispatchToProps( dispatch ) {
+  return { fetchAllRsvps: () => dispatch(fetchAllRsvps()) }
+}
 
-export default MusicContainer
-// export default connect( mapStateToProps, mapDispatchToProps )( MusicContainer )
+function mapStateToProps( state ) {
+  return { rsvps: state.rsvps }
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( MusicContainer )
