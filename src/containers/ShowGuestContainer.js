2bc9615dfde2cbec5a93258016b8ac2769f901a6
torianne02 <victoria.fluharty@gmail.com>
2019-02-05 13:04:59 -0800

    adds class methods for carousel

--- src/containers/ShowGuestContainer.js
+++ src/containers/ShowGuestContainer.js
@@ -1,26 +1,79 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchGuestList } from '../actions/guestListActions';
+import GuestCarousel from '../components/carousel/GuestCarousel';
 
 class ShowGuestContainer extends Component {
-  state = {
-    currentIndex: 0,
-    translateValue: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0
+    };
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === this.props.guestList.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({
+      activeIndex: nextIndex
+    });
+  }
+
+  prev() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? this.props.guestList.length - 1 : this.state.activeIndex - 1;
+    this.setState({
+      activeIndex: nextIndex
+    });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({
+      activeIndex: newIndex
+    })
   }
 
   componentDidMount() {
     this.props.fetchGuestList()
   }
 
-  goToPrevGuest
+  render() {
+    const { activeIndex } = this.state;
+
+    return (
+      <GuestCarousel
+        onExiting={ this.onExiting }
+        onExited={ this.onExited }
+        activeIndex={ activeIndex }
+        next={ this.next }
+        prev={ this.prev }
+        goToIndex={ this.goToIndex }
+        guestList={ this.props.guestList }
+      />
+    )
+  }
 }
 
 const mapStateToProps = state => {
   return { guestList: state.guestList }
 }
 
-const mapDispatchToProps = dispatch => {
-  return { fetchGuestList: () => dispatch( fetchGuestList() )}
-}
+// const mapDispatchToProps = dispatch => {
+//   return { fetchGuestList: () => dispatch( fetchGuestList() )}
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShowGuestContainer)
+export default connect(mapStateToProps, { fetchGuestList })(ShowGuestContainer)
