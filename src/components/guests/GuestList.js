f8eb64412692bda87dce525badab6ca250048915
torianne02 <victoria.fluharty@gmail.com>
2019-02-05 16:35:46 -0800

    changes .map() function in GuestList.js

--- src/components/guests/GuestList.js
+++ src/components/guests/GuestList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Guest from './Guest';
 
-const GuestList = ( {guestList} ) => {
-
+const GuestList = ( guestList ) => {
+  debugger
   return (
     <div className="guest-list">
-      { guestList.map(( guest ) => {
+      { guestList.guestList.map(( guest ) => {
           return <Guest
             key={ guest.id }
             name={ guest.name }
