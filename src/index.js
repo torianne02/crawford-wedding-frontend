f16dbc7ba1cd6514e822a510e5ca630ca38a6ce3
torianne02 <victoria.fluharty@gmail.com>
2019-02-05 18:11:05 -0800

    adds devtools back

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  )
 );
 
 ReactDOM.render(
