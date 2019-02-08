6bdbf5f774581827c69ab63a883333e433ba0bd7
torianne02 <victoria.fluharty@gmail.com>
2019-02-08 11:26:23 -0600

    updates component name in arrow func and exports name

--- src/components/music/MusicList.js
+++ src/components/music/MusicList.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
-const Music = ({ rsvps }) =>
-<div className="music">
-  { rsvps.map(( rsvp ) => {
-    if (rsvp.songRequest != "") {
-      return <Song
-        key={ rsvp.id }
-        name={ rsvp.songRequest }
-      />
-    }
-  }) }
-</div>
+const MusicList = ({ rsvps }) =>
+  <div className="music">
+    { rsvps.map(( rsvp ) => {
+      if (rsvp.songRequest != "") {
+        return <Song
+          key={ rsvp.id }
+          name={ rsvp.songRequest }
+        />
+      }
+    }) }
+  </div>
 
-export default Music
+export default MusicList
