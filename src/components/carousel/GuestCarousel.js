30b9c0b6b275217c59cc5daf23f529e2a3bd6886
torianne02 <victoria.fluharty@gmail.com>
2019-02-08 11:18:21 -0600

    comments out ShowGuestContainer and child component will be a future add on

--- src/components/carousel/GuestCarousel.js
+++ src/components/carousel/GuestCarousel.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption
-} from 'reactstrap';
+// FUTURE WORK TODO
 
-const GuestCarousel = ( props ) => {
-  return (
-    <div>
-
-    </div>
-  );
-}
-
-export default GuestCarousel
+// import React from 'react';
+// import {
+//   Carousel,
+//   CarouselItem,
+//   CarouselControl,
+//   CarouselIndicators,
+//   CarouselCaption
+// } from 'reactstrap';
+//
+// const GuestCarousel = ( props ) => {
+//   const slides = props.guestList.map((guest) => {
+//     return (
+//       <CarouselItem
+//         onExiting={ props.onExiting }
+//         onExited={ props.onExited }
+//         key={ guest.id }
+//       >
+//         <CarouselCaption captionText={ guest.attendees } captionHeader={ guest.name } />
+//       </CarouselItem>
+//     )
+//   })
+//
+//   return (
+//     <Carousel
+//       activeIndex={ props.activeIndex }
+//       next={ props.next }
+//       prev={ props.prev }
+//     >
+//       <CarouselIndicators
+//         guestList={ props.guestList }
+//         activeIndex={ props.activeIndex }
+//         onClickHandler={ props.goToIndex }
+//       />
+//       { slides }
+//       <CarouselControl
+//         direction="prev"
+//         directionText="Previous"
+//         onClickHandler={ props.prev }
+//       />
+//       <CarouselControl
+//         direction="next"
+//         directionText="Next"
+//         onClickHandler={ props.next }
+//       />
+//     </Carousel>
+//   );
+// }
+//
+// export default GuestCarousel
--- src/containers/ShowGuestContainer.js
+++ src/containers/ShowGuestContainer.js
@@ -1,88 +1,83 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchGuestList } from '../actions/guestListActions';
-import GuestCarousel from '../components/carousel/GuestCarousel';
+// FUTURE WORK TODO
 
-class ShowGuestContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeIndex: 0
-    };
-    this.next = this.next.bind(this);
-    this.prev = this.prev.bind(this);
-    this.goToIndex = this.goToIndex.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
-  }
-
-  onExiting() {
-    this.animating = true;
-  }
-
-  onExited() {
-    this.animating = false;
-  }
-
-  next() {
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === this.props.guestList.length - 1 ? 0 : this.state.activeIndex + 1;
-    this.setState({
-      activeIndex: nextIndex
-    });
-  }
-
-  prev() {
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? this.props.guestList.length - 1 : this.state.activeIndex - 1;
-    this.setState({
-      activeIndex: nextIndex
-    });
-  }
-
-  goToIndex(newIndex) {
-    if (this.animating) return;
-    this.setState({
-      activeIndex: newIndex
-    })
-  }
-
-  componentDidMount() {
-    this.props.fetchGuestList()
-  }
-
-  render() {
-    const { activeIndex } = this.state;
-
-    return (
-      <div className="guest-list-carousel">
-        { this.props.guestList.map(( guest ) => {
-            return <GuestCarousel
-              key={ guest.id }
-              name={ guest.name }
-              attendees={ guest.attendees }
-              songRequest={ guest.songRequest }
-              onExiting={ this.onExiting }
-              onExited={ this.onExited }
-              activeIndex={ activeIndex }
-              next={ this.next }
-              prev={ this.prev }
-              goToIndex={ this.goToIndex }
-              guestList={ this.props.guestList }
-            />
-          })
-        }
-      </div>
-    )
-  }
-}
-
-const mapStateToProps = state => {
-  return { guestList: state.guestList }
-}
-
-// const mapDispatchToProps = dispatch => {
-//   return { fetchGuestList: () => dispatch( fetchGuestList() )}
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { fetchGuestList } from '../actions/guestListActions';
+// import GuestCarousel from '../components/carousel/GuestCarousel';
+//
+// class ShowGuestContainer extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       activeIndex: 0
+//     };
+//     this.next = this.next.bind(this);
+//     this.prev = this.prev.bind(this);
+//     this.goToIndex = this.goToIndex.bind(this);
+//     this.onExiting = this.onExiting.bind(this);
+//     this.onExited = this.onExited.bind(this);
+//   }
+// 
+//   onExiting() {
+//     this.animating = true;
+//   }
+//
+//   onExited() {
+//     this.animating = false;
+//   }
+//
+//   next() {
+//     if (this.animating) return;
+//     const nextIndex = this.state.activeIndex === this.props.guestList.length - 1 ? 0 : this.state.activeIndex + 1;
+//     this.setState({
+//       activeIndex: nextIndex
+//     });
+//   }
+//
+//   prev() {
+//     if (this.animating) return;
+//     const nextIndex = this.state.activeIndex === 0 ? this.props.guestList.length - 1 : this.state.activeIndex - 1;
+//     this.setState({
+//       activeIndex: nextIndex
+//     });
+//   }
+//
+//   goToIndex(newIndex) {
+//     if (this.animating) return;
+//     this.setState({
+//       activeIndex: newIndex
+//     })
+//   }
+//
+//   componentDidMount() {
+//     this.props.fetchGuestList()
+//   }
+//
+//   render() {
+//     const { activeIndex } = this.state;
+//
+//     return (
+//       <div className="guest-list-carousel">
+//         <GuestCarousel
+//           onExiting={ this.onExiting }
+//           onExited={ this.onExited }
+//           activeIndex={ activeIndex }
+//           next={ this.next }
+//           prev={ this.prev }
+//           goToIndex={ this.goToIndex }
+//           guestList={ this.props.guestList }
+//         />
+//       </div>
+//     )
+//   }
 // }
-
-export default connect(mapStateToProps, { fetchGuestList })(ShowGuestContainer)
+//
+// const mapStateToProps = state => {
+//   return { guestList: state.guestList }
+// }
+//
+// // const mapDispatchToProps = dispatch => {
+// //   return { fetchGuestList: () => dispatch( fetchGuestList() )}
+// // }
+//
+// export default connect(mapStateToProps, { fetchGuestList })(ShowGuestContainer)
