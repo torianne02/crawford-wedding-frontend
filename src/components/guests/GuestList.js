f9aa14a5a2ec1d52de502bbc19b876116c005c9d
torianne02 <victoria.fluharty@gmail.com>
2019-02-08 12:04:26 -0600

    refactors code/spacing

--- src/components/guests/GuestList.js
+++ src/components/guests/GuestList.js
@@ -4,13 +4,12 @@ import Guest from './Guest';
 const GuestList = ({ guestList }) =>
   <div className="guest-list">
     { guestList.map(( guest ) => {
-        return <Guest
-          key={ guest.id }
-          name={ guest.name }
-          attendees={guest.attendees}
-        />
-      })
-    }
+      return <Guest
+        key={ guest.id }
+        name={ guest.name }
+        attendees={guest.attendees}
+      />
+    }) }
   </div>
 
 export default GuestList
