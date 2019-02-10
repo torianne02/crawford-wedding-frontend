062fa4f9cb41136710221c2e6c772f7b8fa44e55
torianne02 <victoria.fluharty@gmail.com>
2019-02-10 15:50:55 -0600

    adds image to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import background from '../images/background.jpg'
+
+console.log(background)
 
 const Home = () => {
   return (
-
+    <img src={background} alt="Kevin and Tori" />
   )
 }
 
