3d0fbb265e194523a5760d5f401a4d0cfc2341e2
torianne02 <victoria.fluharty@gmail.com>
2019-02-12 12:16:27 -0600

    adds rsvpRequestInProgress to state of rsvpReducer again removed it previously on accident

--- src/reducers/rsvps.js
+++ src/reducers/rsvps.js
@@ -1,10 +1,13 @@
 export default function rsvps( state = {
   rsvp: [],
   rsvps: [],
+  rsvpRequestInProgress: false,
 }, action ) {
   switch ( action.type ) {
+    case 'RSVP_REQUEST':
+      return { ...state, rsvpRequestInProgress: true }
     case 'ADD_RSVP_TO_STATE':
-      return { rsvp: action.data, rsvps: state.rsvps.concat(action.data) }
+      return { rsvpRequestInProgress: false, rsvp: action.data, rsvps: state.rsvps.concat(action.data) }
     case 'FETCH_ALL_RSVPS':
       return { ...state, rsvps: [] }
     case 'ADD_ALL_RSVPS_TO_STATE':
