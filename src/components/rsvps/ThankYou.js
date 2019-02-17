4252c14e2b8957126457f2adcfb395087d7abe91
torianne02 <victoria.fluharty@gmail.com>
2019-02-17 15:58:38 -0800

    changes ThankYou from alert to card with reactstrap also updates styling of margins in App.css

--- src/components/rsvps/ThankYou.js
+++ src/components/rsvps/ThankYou.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert } from 'reactstrap';
+import { Card, CardText, CardBody, CardSubtitle, CardTitle } from 'reactstrap';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux'
 
@@ -8,9 +8,23 @@ class ThankYou extends Component {
     debugger;
     return (
       <div className="thank-you">
-        <Alert color="info">
-          Thank you { this.props.rsvp.name }!
-        </Alert>
+        <Card>
+          <CardBody>
+            <CardTitle>
+              Thank you for your response, { this.props.rsvp.name }.
+            </CardTitle>
+            <CardText>
+              If the below information is incorrect, please reach out to Victoria
+              and/or Kevin.
+            </CardText>
+            <CardSubtitle>RSVP Status:</CardSubtitle>
+            <CardText>{this.props.rsvp.accept}</CardText>
+            <CardSubtitle>Number of Guests Attending:</CardSubtitle>
+            <CardText>{this.props.rsvp.attendees}</CardText>
+            <CardSubtitle>Song Request:</CardSubtitle>
+            <CardText>{this.props.rsvp.song_request}</CardText>
+          </CardBody>
+        </Card>
       </div>
     )
   }
