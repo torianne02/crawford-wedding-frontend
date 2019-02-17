7561d58052e45018ff47d0f848c477ff0b7ff8bf
torianne02 <victoria.fluharty@gmail.com>
2019-02-17 13:52:43 -0800

    imports Link from react-router-dom to NavBar updates links to allow page change without refresh

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Navbar, NavbarBrand, NavItem, NavLink, Nav } from 'reactstrap';
 
 const NavBar = () => {
@@ -7,19 +8,19 @@ const NavBar = () => {
       <NavbarBrand href="/">Crawford Wedding</NavbarBrand>
       <Nav className="nav">
         <NavItem>
-          <NavLink href="/attending">Guest List</NavLink>
+          <NavLink tag={Link} to="/attending">Guest List</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="/rsvp">RSVP</NavLink>
+          <NavLink tag={Link} to="/rsvp">RSVP</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="/details">Details</NavLink>
+          <NavLink tag={Link} to="/details">Details</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="/our-story">Our Story</NavLink>
+          <NavLink tag={Link} to="/our-story">Our Story</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="/music">Music</NavLink>
+          <NavLink tag={Link} to="/music">Music</NavLink>
         </NavItem>
       </Nav>
     </Navbar>
